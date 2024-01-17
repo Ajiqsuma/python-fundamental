4c5fd4c4d5fb870567620b97c520400b22a6cd3a
sumiaji <sumiaji08@gmail.com>
2024-01-17 22:47:51 +0700

    sekuensial dan percabangan sesuai flowcat

--- main.py
+++ main.py
@@ -10,3 +10,22 @@ print('Anak menjawab, "Ok apa yang harus aku lakukan di toko ?"')
 print('Ibu berkata, "Beli satu botol susu"')
 print("Maka anak pergi berangkat ke toko")
 print("Dan mulai berbelanja")
+
+# Percabangan
+jumlah_botol_susu =173
+jumlah_telur = 10
+print(f"jumlah botol susu {jumlah_botol_susu} botol")
+print(f"jumlah telur {jumlah_telur} butir")
+
+if jumlah_botol_susu > 0:
+    print("maka Anak mengecek harga,ternyata uangnya cukup")
+    if jumlah_telur <= 6:
+        print("Anak membeli 1 botol susu")
+    else:
+        print("Anak membeli 1 botol susu ")
+        print("dan 6 butir telur")
+else:
+    print("Anak tidak jadi membeli satu botol")
+
+print("Anak pulang kerumah")
+print("menyampaikan belanjaan ke Ibu")
