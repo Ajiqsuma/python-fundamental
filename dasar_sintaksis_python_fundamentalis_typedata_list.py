4cfb7359fbedd3dbf6a3958849df9e44b472b6f0
sumiaji <sumiaji08@gmail.com>
2024-02-09 22:50:18 +0700

    comprehensif data list baru

--- dasar_sintaksis_python_fundamentalis_typedata_list.py
+++ dasar_sintaksis_python_fundamentalis_typedata_list.py
@@ -60,34 +60,3 @@ daftar_buku.pop(-2)# menghapus elemen kedua dari terakhir
 for i in range (0, len(daftar_buku)):
     print(daftar_buku[i])
 
-print('\nperintah del')
-daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
-del daftar_buku[0]
-for i in range (0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-
-print('\nperintah del dengan list comperhensif')
-daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
-del daftar_buku[:]
-for i in range (0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-print('\nperintah del dengan list comperhensif start')
-daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
-del daftar_buku[0:1]#start:end
-for i in range (0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-print('\nperintah del dengan list comperhensif start step')
-daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
-del daftar_buku[0::2]#start:end:step
-for i in range (0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-print('\nperintah del dengan list comperhensif start')
-daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
-del daftar_buku[0:1]#start:end
-for i in range (0, len(daftar_buku)):
-    print(daftar_buku[i])
-
---
+++ python-fundamental-comprehension-list-data.py
@@ -0,0 +1,62 @@
+print('\nperintah del')
+daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
+del daftar_buku[0]
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+
+print('\nperintah del dengan list comperhensif')
+daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
+del daftar_buku[:]
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nperintah del dengan list comperhensif start')
+daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
+del daftar_buku[0:1]#start:end
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nperintah del dengan list comperhensif start step')
+daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
+del daftar_buku[0::2]#start:end:step
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nperintah del dengan list comperhensif start')
+daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
+del daftar_buku[0:1]#start:end
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nperintah del')
+daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
+del daftar_buku[0]
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+
+print('\nperintah del dengan list comperhensif')
+daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
+del daftar_buku[:]
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nperintah del dengan list comperhensif start')
+daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
+del daftar_buku[0:1]#start:end
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nperintah del dengan list comperhensif start step')
+daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
+del daftar_buku[0::2]#start:end:step
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nmembuat list baru ')
+daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
+daftar_buku_baru = daftar_buku[0:1]
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
