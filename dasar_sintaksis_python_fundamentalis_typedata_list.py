d9598b6cee097b7d510bdc301cafdbb58a177129
sumiaji <sumiaji08@gmail.com>
2024-02-09 22:43:04 +0700

    comprehensif data list

--- dasar_sintaksis_python_fundamentalis_typedata_list.py
+++ dasar_sintaksis_python_fundamentalis_typedata_list.py
@@ -85,3 +85,9 @@ del daftar_buku[0::2]#start:end:step
 for i in range (0, len(daftar_buku)):
     print(daftar_buku[i])
 
+print('\nperintah del dengan list comperhensif start')
+daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
+del daftar_buku[0:1]#start:end
+for i in range (0, len(daftar_buku)):
+    print(daftar_buku[i])
+
