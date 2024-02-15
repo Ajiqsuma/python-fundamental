88249ecd16f12ecd6e613841005a857b7d93581f
sumiaji <sumiaji08@gmail.com>
2024-02-15 21:27:44 +0700

    comprehensif data list baru ganjil genap

--- python_fundamental_comprehension_list_data.py
+++ python_fundamental_comprehension_list_data.py
@@ -54,9 +54,33 @@ del daftar_buku[0::2]#start:end:step
 for i in range (0, len(daftar_buku)):
     print(daftar_buku[i])
 
-print('\nmembuat list baru ')
-daftar_buku = ['Seven Habits','How Influencer people','First Thing First','4DX']
-daftar_buku_baru = daftar_buku[0:1]
+print('\nMembuat list baru ')
+daftar_buku = ['1 Seven Habits','2 How Influencer people','3 First Thing First','4 4DX']
+daftar_buku_baru = daftar_buku[:]
 for i in range (0, len(daftar_buku)):
     print(daftar_buku[i])
+
+print('\nMembuat list baru')
+del daftar_buku [:]
+for i in range (0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nMembuat list baru dengan comprehension  ganjil ')
+daftar_buku = ['1 Seven Habits','2 How Influencer people','3 First Thing First','4 4DX']
+daftar_buku_baru = daftar_buku[0::2]
+for i in range (0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+
+print('\nMembuat list baru dengan comprehension  genap ')
+daftar_buku = ['1 Seven Habits','2 How Influencer people','3 First Thing First','4 4DX']
+daftar_buku_baru = daftar_buku[1::2]
+for i in range (0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+
+print('\nMembuat list baru dengan comprehension  ')
+daftar_buku = ['1 Seven Habits','2 How Influencer people','3 First Thing First','4 4DX']
+print( daftar_buku[1::2])
+
 
